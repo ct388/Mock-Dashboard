5b379741513eea35f371ea03fe295d5af25c365d
Cindy Tr <cindyt388@gmail.com>
2026-08-27 22:59:09 -0700

    Updated vite.config

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Mock-Dashboard",
+  base: "./Mock-Dashboard",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
